1ebd96c5c880bb5c6df6d7b0217512e251aa76ba
Christabel Dapaa-Addo <cdapaaaddo@gmail.com>
2018-06-19 07:44:00 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -30,8 +30,6 @@ def won?(board)
           return winner = [win_index_1, win_index_2, win_index_3]
         if position_1 == "O" && position_2 == "O" && position_3 == "O"
           return winner = [win_index_1, win_index_2, win_index_3]
-        else
-          return false
         end
       end
     end
