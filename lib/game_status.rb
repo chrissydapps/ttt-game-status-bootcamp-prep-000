8fdd153e8f25f87952e1f2f73be0b5f137f790ea
Christabel Dapaa-Addo <cdapaaaddo@gmail.com>
2018-06-19 07:16:10 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,24 +18,24 @@ def won?(board)
   if board.all? {|token| token == " "}
     return false
   else
-  WIN_COMBINATIONS.each do |win_combination|
-    win_index_1 = win_combination[0]
-    win_index_2 = win_combination[1]
-    win_index_3 = win_combination[2]
-    if (position_taken?(board, win_index_1) && position_taken?(board, win_index_2) && position_taken?(board, win_index_3))
-      position_1 = board[win_index_1]
-      position_2 = board[win_index_2]
-      position_3 = board[win_index_3]
-      if position_1 == "X" && position_2 == "X" && position_3 == "X"
-        return winner = [win_index_1, win_index_2, win_index_3]
-      elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-        return winner = [win_index_1, win_index_2, win_index_3]
+    WIN_COMBINATIONS.each do |win_combination|
+      win_index_1 = win_combination[0]
+      win_index_2 = win_combination[1]
+      win_index_3 = win_combination[2]
+      if (position_taken?(board, win_index_1) && position_taken?(board, win_index_2) && position_taken?(board, win_index_3))
+        position_1 = board[win_index_1]
+        position_2 = board[win_index_2]
+        position_3 = board[win_index_3]
+        if position_1 == "X" && position_2 == "X" && position_3 == "X"
+          return winner = [win_index_1, win_index_2, win_index_3]
+        elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+          return winner = [win_index_1, win_index_2, win_index_3]
+        else
+          return winner false
+        end
       else
-        return winner false
+        return false
       end
-    else
-      return false
-    end
   end
 end
 
