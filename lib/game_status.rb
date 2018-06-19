79601f2d8305643a50d94d9f0a3d804f812b43fa
Christabel Dapaa-Addo <cdapaaaddo@gmail.com>
2018-06-19 07:49:32 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -27,8 +27,8 @@ def won?(board)
         position_2 = board[win_index_2]
         position_3 = board[win_index_3]
         if position_1 == "X" && position_2 == "X" && position_3 == "X"
-          return winner = [win_index_1, win_index_2, win_index_3
-            if position_1 == "O" && position_2 == "O" && position_3 == "O"
+          return winner = [win_index_1, win_index_2, win_index_3]
+          if position_1 == "O" && position_2 == "O" && position_3 == "O"
               return winner = [win_index_1, win_index_2, win_index_3]
             end
         end
