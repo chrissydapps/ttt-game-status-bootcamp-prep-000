a6bce2eb3d7f149a7287c9f92cad5a8e2f182c8c
Christabel Dapaa-Addo <cdapaaaddo@gmail.com>
2018-06-19 06:51:02 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,13 +19,15 @@ def won?(board)
     win_index_1 = win_combination[0]
     win_index_2 = win_combination[1]
     win_index_3 = win_combination[2]
-    position_1 = board[win_index_1]
-    position_2 = board[win_index_2]
-    position_3 = board[win_index_3]
-    if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      [win_index_1, win_index_2, win_index_3]
-    else
-      false
+    if (position_taken?(board, win_index_1) && position_taken?(board, win_index_2) && position_taken?(board, win_index_3)
+      position_1 = board[win_index_1]
+      position_2 = board[win_index_2]
+      position_3 = board[win_index_3]
+      if position_1 == "X" && position_2 == "X" && position_3 == "X"
+        [win_index_1, win_index_2, win_index_3]
+      else
+        false
+      end
     end
   end
 end
